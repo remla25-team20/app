ac796dc5728791dc4a1ed6d799bb1bfc2ed3e897
Jeroen Janssen <j.d.janssen@student.tudelft.nl>
2025-06-05 14:28:36 +0200

    Actually dont use middleware, as its only available on edge or in a non-standalone output

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/model-service/:path*',
+        destination: `http://${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
